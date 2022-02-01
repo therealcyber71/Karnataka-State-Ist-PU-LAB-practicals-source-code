d541730f316b1f4919efff14cc89f4b1235f2563
Sachit <72592470+Sachit71@users.noreply.github.com>
2022-02-01 07:59:20 +0530

    Update PROGRAM 1(a).cpp

--- PROGRAM 1(a).cpp
+++ PROGRAM 1(a).cpp
@@ -2,14 +2,14 @@
 #include<conio.h>
 void main( )
 {
-int a, b, temp;
-clrscr( );
-cout<<"Enter the two numbers";
-cin>>a>>b;
-cout<<"Before Interchanging : a = " <<a<<" and b = "<<b<<endl;
-temp = a;
-a = b;
-b = temp;
-cout<<"After Interchanging : a = " <<a<<" and b = "<<b<<endl;
-getch();
+  int a, b, temp;
+  clrscr( );
+  cout<<"Enter the two numbers";
+  cin>>a>>b;
+  cout<<"Before Interchanging : a = " <<a<<" and b = "<<b<<endl;
+  temp = a;
+  a = b;
+  b = temp;
+  cout<<"After Interchanging : a = " <<a<<" and b = "<<b<<endl;
+  getch();
 }
