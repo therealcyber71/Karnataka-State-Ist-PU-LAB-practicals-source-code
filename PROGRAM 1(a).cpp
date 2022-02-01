a637e511800e54b3a2b3a995f544cdacb29c2184
Sachit <72592470+Sachit71@users.noreply.github.com>
2022-02-01 07:58:27 +0530

    Create PROGRAM 1(a).cpp

---
+++ PROGRAM 1(a).cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main( )
+{
+int a, b, temp;
+clrscr( );
+cout<<"Enter the two numbers";
+cin>>a>>b;
+cout<<"Before Interchanging : a = " <<a<<" and b = "<<b<<endl;
+temp = a;
+a = b;
+b = temp;
+cout<<"After Interchanging : a = " <<a<<" and b = "<<b<<endl;
+getch();
+}
